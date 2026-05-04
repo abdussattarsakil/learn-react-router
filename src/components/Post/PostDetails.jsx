b9a5fcc1f2c0820d50ac5c5224480085851e804b
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-05-04 14:12:18 +0600

    learn use navigate

--- src/components/Post/PostDetails.jsx
+++ src/components/Post/PostDetails.jsx
@@ -1,12 +1,15 @@
-import { useLoaderData } from "react-router";
+import { useLoaderData, useNavigate } from "react-router";
 
 const PostDetails = () => {
     const post=useLoaderData();
+    const navigate= useNavigate();
     const {body,title}=post
     return (
         <div>
             <h1>{title}</h1>
             <h1>{body}</h1>
+            <br />
+            <button onClick={()=>navigate(-1)}>GO BACK</button>
         </div>
     );
 };
