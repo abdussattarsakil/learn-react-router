d4335bb7f66e18e391f5a170b1b34e40956332f9
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-05-04 14:20:50 +0600

    learn use navigation for loading spinner

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router';
+import { Outlet, useNavigation } from 'react-router';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Side from '../SideBar/Side';
@@ -6,11 +6,14 @@ import Side from '../SideBar/Side';
 import "./Root.css"
 
 const Root = () => {
+    const navigation = useNavigation();
+    const navigating = Boolean(navigation.location)
     return (
         <div>
             <Header></Header>
             <div className='side-bar'>
                 <Side></Side>
+                { navigating && <span>useNavigation loading.......</span>}
                 <Outlet></Outlet>
             </div>
             <Footer></Footer>
